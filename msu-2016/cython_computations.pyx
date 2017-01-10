1b04954a138d22aa035fa55c9da5a768d0591d8e
Gaurav Baruah <gbaruah@uwaterloo.ca>
2017-01-10 13:48:27 -0500

    intermediate heap code; NOTE: "starting heap code" commit is a good one

--- msu-2016/cython_computations.pyx
+++ msu-2016/cython_computations.pyx
@@ -5,7 +5,11 @@ from collections import defaultdict
 import bisect
 import heapq
 import operator
+from libc.stdlib cimport malloc, free
+from cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
+from libcpp.vector cimport vector
 
+from sys import maxint
 
 # import logging
 # logger = logging.getLogger(__name__)
@@ -15,35 +19,74 @@ import operator
 # ch.setFormatter(formatter)
 # logger.addHandler(ch)
 
-#@cython.profile(True)
-@cython.boundscheck(False)
-@cython.wraparound(False)
-cdef bint heap_top_is_smaller(heap, double upd_time, double upd_conf, int upd_idx):
-    """
-    top: (confidence, time, index)
-    update: update object
-    """
-    if not heap:
-        return True
-    # top_conf, top_time, top_index, top_wlen = heap[0]
-    if heap[0][0] < upd_conf:
-        return True
-    if abs(heap[0][0] - upd_conf) < 1e-8:
-        if heap[0][1] < upd_time:
-            return True
-        if abs(heap[0][1] - upd_time) < 1e-8:
-            return heap[0][2] < upd_idx
-    return False
+cdef struct UpdateData:
+    double conf
+    double time
+    int index
+    double wlen
 
-#@cython.profile(True)
-@cython.boundscheck(False)
-@cython.wraparound(False)
-cdef void add_to_heap(topkqueue, int topkcount, int upd_idx, double upd_time, double upd_conf, double upd_wlen):
-    if len(topkqueue) < topkcount:
-        heapq.heappush( topkqueue, (upd_conf, upd_time, upd_idx, upd_wlen) )    
-    elif topkcount >0 and len(topkqueue) == topkcount and heap_top_is_smaller(topkqueue, upd_time, upd_conf, upd_idx) :                            
-        heapq.heappushpop( topkqueue, (upd_conf, upd_time, upd_idx, upd_wlen) )
-    #assert(len(topkqueue) <= topkcount)
+    
+
+cdef class UpdateHeap:
+    cdef vector[UpdateData] minheap
+    cdef vector[UpdateData] maxheap
+    cdef int heap_size
+    cdef int max_heap_size
+    cdef int topkcount
+
+    def __init__(self, int max_heap_size):
+        
+        for i in xrange(max_heap_size):
+            self.minheap.push_back(UpdateData(float("inf"),float("inf"),maxint,float("inf")))
+            self.maxheap.push_back(UpdateData(float("-inf"), float("-inf"), -1, float("-inf")))
+        self.heap_size = 0
+        self.topkcount = 0        
+
+    cdef int update_topkcount(self, incr):
+        self.topkcount += incr
+        return self.topkcount
+
+    cdef minheappush(self, upd_data):
+        pass
+
+    cdef minheappushpop(self, upd_data):
+        pass
+
+    cdef re_minheapify(self, time_filter):
+        for upddata in self.maxheap:                            
+            if upddata.time > time_filter:
+                self.minheappush(upddata)
+        pass
+    
+    cdef maxheapify(self):
+        pass
+
+    cdef popmax(self):
+        pass
+    
+    cdef bint heap_top_is_smaller(self, double upd_time, double upd_conf, int upd_idx):
+        """
+        top: (confidence, time, index)
+        update: update object
+        """
+        if not self.minheap.size():
+            return True
+        # top_conf, top_time, top_index, top_wlen = heap[0]
+        if self.minheap[0].conf < upd_conf:
+            return True
+        if abs(self.minheap[0].conf - upd_conf) < 1e-8:
+            if self.minheap[0].time < upd_time:
+                return True
+            if abs(self.minheap[0].time - upd_time) < 1e-8:
+                return self.minheap[0].index < upd_idx
+        return False
+
+    cdef void add_to_heap(self, int upd_idx, double upd_time, double upd_conf, double upd_wlen):
+        if self.heap_size < self.topkcount:
+            self.minheappush( UpdateData(upd_conf, upd_time, upd_idx, upd_wlen) )            
+        elif self.topkcount >0 and self.heap_size == self.topkcount and self.heap_top_is_smaller(upd_time, upd_conf, upd_idx) :
+            self.minheappushpop( UpdateData(upd_conf, upd_time, upd_idx, upd_wlen) )        
+    
 
 #@cython.profile(True)
 @cython.boundscheck(False)
@@ -53,21 +96,18 @@ cdef process_session(updates_read, already_seen_ngts, updates,
                 double ssn_start, int ssn_reads, int uti,
                 double next_ssn_start, double next_ssn_window_start,
                 ssn_starts, # needed for alpha computation
-                topkqueue, topkcount):
+                UpdateHeap topkqueue):
     
     cdef double session_msu = 0.0
     cdef double current_time = 0.0
 
     # logger.debug('session {}: start {}; reads {}'.format(uti, ssn_start, ssn_reads))
 
-    available_updates = sorted(topkqueue, key=operator.itemgetter(0), reverse=True)                
+    #available_updates = sorted(topkqueue, key=operator.itemgetter(0), reverse=True)  
+    topkqueue.maxheapify()              
+        
+    topkqueue.update_topkcount(-ssn_reads)
     
-    topkqueue = available_updates[ssn_reads:]
-    heapq.heapify(topkqueue)
-    topkcount -= ssn_reads
-    #assert(topkcount >= 0)                
-
-    available_updates = available_updates[:ssn_reads]
     # logger.debug('available_updates {}'.format(available_updates))
     # logger.debug('topk count {} queue {}'.format(topkcount, topkqueue))
 
@@ -79,9 +119,13 @@ cdef process_session(updates_read, already_seen_ngts, updates,
     cdef int ngt_after = 0
     cdef double ngt_msu = 0.0
 
-    for ai in xrange(len(available_updates)): # for num_reads
-        
-        read_update_wlen = available_updates[ai][3]
+    #for ai in xrange(len(available_updates)): # for num_reads
+    while ssn_reads >0:
+        ssn_reads -= 1
+
+        upddata = topkqueue.maxheap[0]
+
+        read_update_wlen = upddata.wlen
         
         upd_time_to_read = (float(read_update_wlen) / user_reading_speed)
         current_time += upd_time_to_read
@@ -90,20 +134,13 @@ cdef process_session(updates_read, already_seen_ngts, updates,
             # user persisted in reading upto the start of the next session
             # logger.info('user persisted in reading upto the start of the next session')
             
-            for ti in xrange(ai, len(available_updates)):                            
-                if available_updates[ti][1] > next_ssn_window_start:
-                    add_to_heap(topkqueue, topkcount, 
-                        available_updates[ti][2],
-                        available_updates[ti][0], 
-                        available_updates[ti][1],
-                        available_updates[ti][3]
-                        )
             break
         
-        updates_read[available_updates[ai][2]] = True
+        updates_read[upddata.index] = True
+        topkqueue.popmax()
         # logger.debug('read update {}'.format(read_update))
         
-        read_update = updates[available_updates[ai][2]]
+        read_update = updates[upddata.index]
         read_update_msu = 0.0
         # check for nuggets and update user msu
         for ngt in read_update.nuggets:
@@ -119,7 +156,10 @@ cdef process_session(updates_read, already_seen_ngts, updates,
         
         session_msu += read_update_msu
     # logger.debug('processed session msu ={}'.format(session_msu))
-    return session_msu, topkqueue, topkcount
+
+    topkqueue.re_minheapify(next_ssn_window_start)
+    return session_msu
+
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -159,11 +199,14 @@ def _compute_ranked_user_MSU(user_trail, window_starts, ssn_starts,
     cdef int num_updates = len(update_times)    
     cdef int num_sessions = len(user_trail)
             
-    topkqueue = []
+    #topkqueue = []
     cdef int topkcount = 0
     
     max_heap_size = find_max_heap_size(user_trail, window_starts, num_sessions)
     # logger.debug('max_heap_size {}'.format(max_heap_size))
+    #cdef UpdateData *topkqueue = <UpdateData*>malloc(max_heap_size * sizeof(UpdateData))
+    cdef UpdateHeap topkqueue = UpdateHeap(max_heap_size);
+    
 
     cdef int uti = 0
     cdef int wsi = 0
@@ -185,7 +228,8 @@ def _compute_ranked_user_MSU(user_trail, window_starts, ssn_starts,
 
         # check for window starts
         while wsi < num_sessions and window_starts[wsi] < update_times[upd_idx]:
-            topkcount += user_trail[wsi][1]
+            #topkcount += user_trail[wsi][1]
+            topkqueue.update_topkcount(user_trail[wsi][1])
             # logger.debug('window {} started; needs {} '.format(wsi, user_trail[wsi][1]))
             wsi += 1
         # logger.debug('topkcount {}'.format(topkcount))
@@ -199,12 +243,12 @@ def _compute_ranked_user_MSU(user_trail, window_starts, ssn_starts,
             next_ssn_window_start = window_starts[uti+1] if uti +1 != num_sessions else query_duration
             
             
-            session_msu, topkqueue, topkcount = process_session(updates_read, already_seen_ngts, updates,
+            session_msu = process_session(updates_read, already_seen_ngts, updates,
                                                     user_reading_speed, user_latency_tolerance,
                                                     ssn_start, ssn_reads, uti,
                                                     next_ssn_start, next_ssn_window_start,
                                                     ssn_starts,
-                                                    topkqueue, topkcount)
+                                                    topkqueue)
             user_topic_msu += session_msu
             uti += 1
 
@@ -212,7 +256,7 @@ def _compute_ranked_user_MSU(user_trail, window_starts, ssn_starts,
             # logger.debug('all sessions processed')
             break
 
-        add_to_heap(topkqueue, topkcount, upd_idx,
+        topkqueue.add_to_heap(upd_idx,
             update_times[upd_idx], update_confidences[upd_idx], update_lengths[upd_idx])
         # logger.debug('adding update {} to queue'.format(upd_idx))
         ## logger.debug('topkqueue {}'.format(topkqueue))
@@ -223,12 +267,12 @@ def _compute_ranked_user_MSU(user_trail, window_starts, ssn_starts,
         # logger.debug('all updates processed.')        
     while uti < num_sessions:
         # logger.debug('processing leftover session')
-        session_msu, topkqueue, topkcount = process_session(updates_read, already_seen_ngts, updates,
+        session_msu = process_session(updates_read, already_seen_ngts, updates,
                                                 user_reading_speed, user_latency_tolerance,
                                                 ssn_start, ssn_reads, uti,
                                                 next_ssn_start, next_ssn_window_start,
                                                 ssn_starts,
-                                                topkqueue, topkcount)
+                                                topkqueue)
         user_topic_msu += session_msu
         if len(updates_read) == num_updates:
             # logger.debug('read all updates')
@@ -241,5 +285,6 @@ def _compute_ranked_user_MSU(user_trail, window_starts, ssn_starts,
     # logger.debug( str(user_instance) )
     # logger.debug(user_topic_msu)
 
+    #free(topkqueue)
     
     return user_topic_msu
--- msu-2016/setup.py
+++ msu-2016/setup.py
@@ -3,5 +3,7 @@ from Cython.Build import cythonize
 
 setup(
         name = 'cython msu computations',
-        ext_modules = cythonize('cython_computations.pyx')
+        ext_modules = cythonize(
+                'cython_computations.pyx',
+                language='c++')
 )
